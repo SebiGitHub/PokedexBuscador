b6ef89490bb30f16d19b31e00884b5ccc461af11
sebit <sebitaexporu@gmail.com>
2025-02-09 17:54:31 +0100

    Me falta la parte de detallles

--- app/src/main/java/com/example/brawlbuscador/ApiService.kt
+++ app/src/main/java/com/example/brawlbuscador/ApiService.kt
@@ -7,19 +7,8 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("pokemon/{name}")
-    suspend fun getPokemonName(@Path("name") pokemonName: String): Response<PokemonsListNames>
-
-    @GET("pokemon/{name}")
-    suspend fun getPokemonSprites(@Path("name") pokemonName: String): Response<PokemonsListSprites>
-
-    @GET("pokemon/{name}")
-    suspend fun getPokemonId(@Path("name") pokemonName: String): Response<PokemonId>
-
-
     @GET("pokemon")
     suspend fun getPokemonList(@Query("limit") limit: Int): Response<PokemonListResponse>
-
 
     //Detail
     @GET("pokemon/{id}")
--- app/src/main/java/com/example/brawlbuscador/PokemonDataResponse.kt
+++ app/src/main/java/com/example/brawlbuscador/PokemonDataResponse.kt
@@ -2,10 +2,6 @@ package com.example.brawlbuscador
 
 import com.google.gson.annotations.SerializedName
 
-// Respuesta básica al buscar un Pokémon
-data class PokemonDataResponse(
-    @SerializedName("types") val types: List<PokemonTypeResponse>,  // Tipos del Pokémon
-)
 
 data class PokemonData(
     val name: String,
@@ -21,43 +17,4 @@ data class PokemonListResponse(
 data class PokemonNameResponse(
     @SerializedName("name") val name: String,
     @SerializedName("url") val url: String
-)
-
-
-
-//Id
-data class PokemonId(
-    @SerializedName("id") val id: Int  // ID único del Pokémon
-)
-
-
-//Sprite
-data class PokemonsListSprites(
-    @SerializedName("sprites") val sprites: PokemonSprites // Imagen del Pokémon)
-)
-
-data class PokemonSprites(
-    @SerializedName("front_default") val frontDefault: String // Imagen frontal del Pokémon
-)
-
-
-//Name
-data class PokemonsListNames(
-    @SerializedName("form") val form: List<PokemonNames> // Lista de Pokémon
-)
-
-data class PokemonNames(
-    @SerializedName("name") val name: String // Nombre del Pokémon
-)
-
-
-
-// Respuesta de los tipos del Pokémon (por ejemplo, fuego, agua, etc.)
-data class PokemonTypeResponse(
-    @SerializedName("type") val type: PokemonTypeDetail           // Detalles sobre el tipo
-)
-
-// Detalles del tipo (fuego, agua, etc.)
-data class PokemonTypeDetail(
-    @SerializedName("name") val name: String                      // Nombre del tipo (ej. "fire", "water")
 )
